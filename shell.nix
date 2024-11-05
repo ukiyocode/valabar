5958396bc0c24c8c87b41e1604e945c59b52170c
ukiyocode <77393110+ukiyocode@users.noreply.github.com>
2024-11-05 12:03:56 +0100

    working nix SHELL environment (¬_¬")

---
+++ shell.nix
@@ -0,0 +1,14 @@
+{ pkgs ? import <nixpkgs> {} }:
+  pkgs.mkShell {
+    # nativeBuildInputs is usually what you want -- tools you need to run
+    nativeBuildInputs = with pkgs.buildPackages; [ 
+      glib
+      gobject-introspection
+      gtk3
+      libwnck
+      vala
+    ];
+    shellHook = ''
+      exec zsh
+    '';
+}

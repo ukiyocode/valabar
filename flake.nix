665af1814abe601fc5eaa283784b20958f3c7da0
ukiyocode <77393110+ukiyocode@users.noreply.github.com>
2024-11-07 18:52:41 +0100

    some more nix stuff

--- flake.nix
+++ flake.nix
@@ -1,27 +1,21 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/92d295f588631b0db2da509f381b4fb1e74173c5";
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
   };
 
   outputs = { self, nixpkgs }:
     let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
+      sys = "x86_64-linux";
     in
     {
-      devShells.${system}.default = with pkgs; mkShell {
+      devShells.${sys}.default = with nixpkgs.legacyPackages.${sys}; mkShell {
         buildInputs = [
           glib
           gobject-introspection
           gtk3
           libwnck
           vala
-          zsh
         ];
-        /*shellHook =
-        ''
-          exec zsh
-        '';*/
       };
     };
 }
